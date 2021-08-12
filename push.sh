d13ffb87bbfe3c60b7710b9fcc17e8e36a55a81d
David R <egoz3ntrum@gmail.com>
2021-08-12 14:21:43 +0200

    auto commit by darodrig on c1r12s6

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-git add .
-git commit -m "auto commit by darodrig on `echo $(uname -n)`"
+FILES="bin level0* "
+git add $FILES
+git commit -m "auto commit by darodrig on `echo $(uname -n) | cut -d. -f1`"
 git push origin main
